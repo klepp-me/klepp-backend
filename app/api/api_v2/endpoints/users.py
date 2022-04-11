81d54799bb6ec9f51523963512f1e47d480068bc
Jonas Krüger Svensson <jonas.svensson@intility.no>
2022-04-11 18:28:28 +0200

    Add list users API

--- app/api/api_v2/endpoints/users.py
+++ app/api/api_v2/endpoints/users.py
@@ -7,22 +7,22 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.api.dependencies import yield_db_session
 from app.api.security import cognito_scheme_or_anonymous
-from app.models.klepp import ListResponse, Tag, TagRead
+from app.models.klepp import ListResponse, User, UserRead
 
 router = APIRouter()
 
 
-@router.get('/tags', response_model=ListResponse[TagRead], dependencies=[Depends(cognito_scheme_or_anonymous)])
-async def get_all_tags(
+@router.get('/users', response_model=ListResponse[UserRead], dependencies=[Depends(cognito_scheme_or_anonymous)])
+async def get_users(
     session: AsyncSession = Depends(yield_db_session),
     offset: int = 0,
     limit: int = Query(default=100, lte=100),
 ) -> dict[str, int | list]:
     """
-    Gets possible tags to use
+    Get a list of users
     """
-    # Video query
-    tag_statement = select(Tag).order_by(desc(Tag.name))
+    # User query
+    tag_statement = select(User).order_by(desc(User.name))
     # Total count query based on query params, without pagination
     count_statement = select(func.count('*')).select_from(tag_statement)  # type: ignore
 
